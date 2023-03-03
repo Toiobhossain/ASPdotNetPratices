f7925dde1dba3606c5c73b1f035f3659d278cf5c
toiobhossain <tusharahmed16135@gmail.com>
2023-03-04 01:01:35 +0600

    Add Car Model Class

---
+++ FirstCoreApplication/Models/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstCoreApplication.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Color { get; set; }
+        public String Brand { get; set; }
+        public String RegisterNo { get; set; }
+        public decimal Price { get; set; }
+    }
+}
